26f9f1e9b8e1ae2fe16396afa6dd46363030f0e9
Smertsss <boldenkoa@bk.ru>
2025-03-02 17:31:41 +0300

    add migration

--- src/main/java/com/boldenko/game_shop_api/repository/ClientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.boldenko.game_shop_api.repository;
-
-import com.boldenko.game_shop_api.entity.Client;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface ClientRepo extends CrudRepository<Client, UUID> {
-}
