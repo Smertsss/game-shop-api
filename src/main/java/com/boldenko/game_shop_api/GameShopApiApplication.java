a1a0a168e56cd5a9019d9b4e5ef35ced01a80b8e
Smertsss <boldenkoa@bk.ru>
2025-03-02 14:24:29 +0300

    Initial commit

---
+++ src/main/java/com/boldenko/game_shop_api/GameShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.boldenko.game_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameShopApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/boldenko/game_shop_api/mapper/DataMapper.java
@@ -0,0 +1,7 @@
+package com.boldenko.game_shop_api.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DataMapper {
+}
